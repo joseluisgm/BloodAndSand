01d7fe7f659c75d9d5508ddcceaa23b3b951f051
joseluisgm <46574233+joseluisgm@users.noreply.github.com>
2020-05-14 17:29:00 +0200

    intentado hacer los disparos
    no salen ni queriendo necestio mas tiempo

---
+++ Blood and sand/Assets/Scripts/disparo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class disparo : MonoBehaviour
+{
+    //tengo que buscar mas como hacerlo porque esto no sale 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
