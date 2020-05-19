06b17db8d668fafabc7bee12368c162a3ad1174d
joseluisgm <46574233+joseluisgm@users.noreply.github.com>
2020-05-19 18:26:23 +0200

    funcion de desparar metida
    funciona aun un poco raro pero fufa

--- Blood and sand/Assets/Scripts/disparo.cs
+++ Blood and sand/Assets/Scripts/disparo.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class disparo : MonoBehaviour
 {
-    //tengo que buscar mas como hacerlo porque esto no sale 
+    public static float v = 5f;
+    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.AddForce(v, 0f, 0f);
+        StartCoroutine(eliminar(2f));
+    }
+
+    IEnumerator eliminar(float s)
+    {
+        yield return new WaitForSeconds(s);
+        Destroy(gameObject);
     }
 }
--- Blood and sand/Assets/Scripts/mover.cs
+++ Blood and sand/Assets/Scripts/mover.cs
@@ -8,14 +8,19 @@ public class mover : MonoBehaviour
     // Start is called before the first frame update
     float v = 5;
     float s = 6.5f;
+    public GameObject proyectil;
+    public GameObject posDisparo1;
+    public GameObject posDisparo2;
+    GameObject spawn;
     void Start()
     {
-        
+        spawn = posDisparo1;
     }
     // Update is called once per frame
     void FixedUpdate()
     {
         direccion();
+        disparar();
     }
     private void direccion()
     {
@@ -26,10 +31,22 @@ public class mover : MonoBehaviour
         if (Input.GetKey(KeyCode.A))
         {
             this.transform.Translate(new Vector2( -v * Time.deltaTime,0));
+            spawn = posDisparo2;
+            disparo.v = -5f;
         }
         if (Input.GetKey(KeyCode.D))
         {
             this.transform.Translate(new Vector2( v * Time.deltaTime,0));
+            spawn = posDisparo1;
+            disparo.v = 5f;
+        }
+    }
+
+    private void disparar()
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            Instantiate(proyectil,spawn.transform.position,transform.rotation);
         }
     }
 
